cc0e62d3dea45ddc6106a5b8ad9ad147c160a33a
ujjawal10024 <ujjawalshakya94@gmail.com>
2025-11-22 15:21:41 +0530

    Day 1: basic Python programs (input + table)

---
+++ lessgo.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print(f"You will be {age + 5} years old after 5 years")
---
+++ table2.py
@@ -0,0 +1,5 @@
+num = int(input("Number: "))
+limit = int(input("Kaha tak ka table chahiye? "))
+
+for i in range (1,limit + 1):
+    print (f"{num} x {i} = {num * i}")
